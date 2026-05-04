cdcef8c4c49076d6fe7312d77421a450f81f3416
aljaziz <turjatheharbingerofdeath@gmail.com>
2026-05-04 21:27:07 +0600

    api response type added

--- backend/src/types.ts
+++ backend/src/types.ts
@@ -17,3 +17,23 @@ export interface Surah {
     revelationType: string;
     ayahs?: Ayah[];
 }
+
+export interface ApiSurahResponse {
+    code: number;
+    status: string;
+    data: {
+        number: number;
+        name: string;
+        englishName: string;
+        englishNameTranslation: string;
+        numberOfAyahs: number;
+        revelationType: string;
+        ayahs: Array<{
+            number: number;
+            numberInSurah: number;
+            text: string;
+            juz: number;
+            page: number;
+        }>;
+    };
+}
